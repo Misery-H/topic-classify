d5161bae9f2b12ccdb1110cb37d0e4e2170d5be6
10401 <10401@10401.com>
2024-11-18 00:17:20 +0800

    RESULT

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ def preprocess_text(text):
 if __name__ == '__main__':
 
     data = pd.read_csv('data.csv', encoding='utf-8', header=None)
-    #读取data的第二列
+    # 读取data的第二列和第五列
+    original_data = data.iloc[:, [1, 4]]
     data = data.iloc[:, 1]
-    #将data的第二列的数据进行预处理
+    # 将data的第二列的数据进行预处理
     data['processed_text'] = data.apply(preprocess_text)
     texts = data['processed_text'].tolist()
 
@@ -46,14 +47,28 @@ if __name__ == '__main__':
     corpus = [dictionary.doc2bow(text) for text in texts]
 
     # 训练LDA模型
-    lda_model = models.LdaModel(corpus=corpus, id2word=dictionary, random_state=4583, chunksize=20, num_topics=7, passes=200, iterations=400)
+    lda_model = models.LdaModel(corpus=corpus, id2word=dictionary, random_state=4583, chunksize=5, num_topics=5,
+                                passes=200, iterations=500)
 
     # 打印LDA主题
-    for topic in lda_model.print_topics(num_topics=7, num_words=10):
+    for topic in lda_model.print_topics(num_topics=7, num_words=3):
         print(topic)
     coherence_model = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
     coherence_score = coherence_model.get_coherence()
     print(coherence_score)
+    topic_files = {}
+    for topic_id in range(lda_model.num_topics):
+        topic_files[topic_id] = open(f'topic_{topic_id}.txt', 'w', encoding='utf-8')
+
+    for i, text in enumerate(texts):
+        topic_distribution = lda_model.get_document_topics(dictionary.doc2bow(text), minimum_probability=0)
+        dominant_topic = max(topic_distribution, key=lambda x: x[1])[0]
+        # 写入原始文本
+        topic_files[dominant_topic].write(
+            ''.join(original_data.iloc[i, 0]) + '\t' + str(original_data.iloc[i, 1]) + '\n')
+
+    for file in topic_files.values():
+        file.close()
 
     dickens_visual = gensimvisualize.prepare(lda_model, corpus, dictionary, mds='mmds')
     pyLDAvis.display(dickens_visual)
